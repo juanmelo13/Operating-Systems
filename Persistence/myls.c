fbcdcdb44a003e3ecf66e7d34dec729e8c8a4b7a
Juan Melo <61530652+juanmelo13@users.noreply.github.com>
2021-04-27 01:52:13 +0000

    Update myls.c

--- Persistence/myls.c
+++ Persistence/myls.c
@@ -3,20 +3,35 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
 
 int main(int argc, char *argv[]) {
-    if (argc > 4 && argc < 1) {                        // Check if command is used successfully
-        printf("Usage: myls -OPTIONS DIRNAME\n");
+    if (argc > 3 && argc < 1) {                             // Check if command is used successfully
+        printf("Usage: myls (-OPTIONS) (DIRNAME)\n");
         return 0;
     }
 
-    char* path = ".";
-    int longer = strcmp("-l", argv[1]);
-    printf("%i\n", longer);
+    int longer = (argc > 1) ? strcmp("-l", argv[1]) : -1;   // -l command used?
+    char* path;                                         
 
-    if (longer == 0) {
-        
+    // Check if -OPTIONS argument is passed, assign path to desired path ("." if ommitted)
+    if (longer == 0) 
+        path = (argc == 3) ? argv[2] : ".";
+    else 
+        path = (argc == 2) ? argv[1] : ".";
+
+    
+    DIR *d;                                                 // Directory pointer
+    if ((d = opendir(path)) == NULL) {
+        printf("Unable to open %s\n", path);
+        return 0;
+    }
+    struct dirent *entry;  
+
+    while ((entry = readdir(d)) != NULL) {
+        printf("%s\n", entry->d_name);
     }
 
+    closedir(d);
     return 0;
 }
