841fd3674bbe709b14e59cca669b449902f0abbf
Juan Melo <61530652+juanmelo13@users.noreply.github.com>
2021-04-29 21:36:27 +0000

    Add files via upload

--- Persistence/myls.c
+++ Persistence/myls.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <time.h>
 
 void printStat(char *PATH);
 
@@ -24,17 +26,19 @@ int main(int argc, char *argv[]) {
 
     
     DIR *d;                                                 // Directory pointer
-    if ((d = opendir(path)) == NULL) {
+    if ((d = opendir(path)) == NULL) {                      // Try opening the directory
         printf("Unable to open %s\n", path);
         return 0;
     }
     struct dirent *entry;  
 
+    // Iterate over the files in the directory
     while ((entry = readdir(d)) != NULL) {
         if (longer == 0) {
             printStat(entry->d_name);
+            printf(" %s\n", entry->d_name);
         }
-        printf("%s\n", entry->d_name);
+        else printf("%-10s", entry->d_name);
     }
 
     closedir(d);
@@ -42,5 +46,38 @@ int main(int argc, char *argv[]) {
 }
 
 void printStat(char *PATH) {
-    
+    struct stat statBuff;                   // Buffer to store the stats
+    struct tm *time;                        // Time
+    char timeStr[40];
+    if (stat(PATH, &statBuff) == -1) printf("Unable to get stats for %s", PATH);
+
+    // Print file stats
+    else {
+        // Print permissions.
+        printf((S_ISDIR(statBuff.st_mode)) ? "d" : "-");
+        printf((statBuff.st_mode & S_IRUSR) ? "r" : "-");
+        printf((statBuff.st_mode & S_IWUSR) ? "w" : "-");
+        printf((statBuff.st_mode & S_IXUSR) ? "x" : "-");
+        printf((statBuff.st_mode & S_IRGRP) ? "r" : "-");
+        printf((statBuff.st_mode & S_IWGRP) ? "w" : "-");
+        printf((statBuff.st_mode & S_IXGRP) ? "x" : "-");
+        printf((statBuff.st_mode & S_IROTH) ? "r" : "-");
+        printf((statBuff.st_mode & S_IWOTH) ? "w" : "-");
+        printf((statBuff.st_mode & S_IXOTH) ? "x" : "-");
+
+        // Print links
+        printf(" %lu ", statBuff.st_nlink);
+
+        // Print owner and group
+        printf("%u ", statBuff.st_uid);
+        printf("%u ", statBuff.st_gid);
+
+        // Print time
+        time = localtime(&statBuff.st_mtime);
+        strftime(timeStr, 40, "%m %d %H:%M", time);
+        printf("%s ", timeStr);
+
+        // Print size
+        printf("%6lu", statBuff.st_size);      
+    }
 }
--- Persistence/mytail.c
+++ Persistence/mytail.c
@@ -2,12 +2,42 @@
 // By: Juan Melo
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2 && argc != 1) {                        // Check if command is used successfully
-        printf("Usage: myls -OPTIONS\n");
+    if (argc != 3) {                        // Check if command is used successfully
+        printf("Usage: mytail -n FILENAME\n");
         return 0;
     }
 
+    int file;                               // File object
+    struct stat statBuff;                   // Buffer to store the size
+    char stringBuff[100];                   // String buffer to print the tail
+
+    char *path = argv[2];
+    int n = atoi(argv[1]);
+
+    if (stat(path, &statBuff) == -1) printf("Unable to get stats for %s", path);
+    unsigned long fileSize = statBuff.st_size;
+
+    file = open(path, O_RDONLY);                // Open file and check if it was opened successfully
+    if (file < 0) {
+        printf("Could not open file %s\n", path);
+        return -1;
+    }
+
+    lseek(file, fileSize, SEEK_SET);        // Go to end of file
+    // while (n > 0) {
+
+    lseek(file, -100, SEEK_CUR);
+    while (read(file, stringBuff, 100)) printf("%s", stringBuff);
+    printf("\n%lu\n%i\n", fileSize, n);
+
+
+    close(file);
     return 0;   
 }
